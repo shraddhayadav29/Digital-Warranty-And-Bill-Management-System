b262d445184f6f4de52041598694bd336573a537
shraddhayadav29 <yadavshraddha2905@gmail.com>
2026-03-29 14:10:01 +0530

    updated settings

--- digitalwarranty/settings.py
+++ digitalwarranty/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 
+SECRET_KEY = os.environ.get('Shraddha123', 'fallback-secret')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
