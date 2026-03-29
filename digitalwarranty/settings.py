bcce530732956cae466ed8d0b8ba1f697bb05d2f
shraddhayadav29 <yadavshraddha2905@gmail.com>
2026-03-29 15:15:21 +0530

    Updated settings

--- digitalwarranty/settings.py
+++ digitalwarranty/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&yn!#qu015is+@7@r-=b0*@@$begg5(=_!#eloi6vki0m_r0p_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
