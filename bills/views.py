2e9faaa9345ff34e1cae8f5b845e86782e77e738
shraddhayadav29 <yadavshraddha2905@gmail.com>
2026-04-01 23:02:06 +0530

    create admin

--- bills/views.py
+++ bills/views.py
@@ -195,6 +195,7 @@ def home(request):
 
 
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 def create_admin(request):
     if not User.objects.filter(username="admin").exists():
