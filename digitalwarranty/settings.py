b551fd300ac4ec4294d4c08ff4bbf2612844cd63
shraddhayadav29 <yadavshraddha2905@gmail.com>
2026-04-02 10:15:40 +0530

    updated email

--- digitalwarranty/settings.py
+++ digitalwarranty/settings.py
@@ -126,14 +126,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'yadavshraddha2905@gmail.com'
-EMAIL_HOST_PASSWORD = 'vdbkcmtsyklxpevl'
-LOGIN_URL = '/accounts/login/'
+import os
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('vdbk cmts yklx pevl')
+
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
